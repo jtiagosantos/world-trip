9b2fbe97e34d2433fad77305661c3fec8ab7da5d
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-11 09:07:35 -0300

    feat: add divider and text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head';
+import { Flex, Divider, Text } from '@chakra-ui/react';
 import { Banner } from '../components/Banner/Banner';
 import { Header } from '../components/Header/Header';
 import { TravelTypes } from '../components/TravelTypes/TravelTypes';
 
 export default function Home() {
   return (
-    <>
+    <Flex flexDirection="column" align="center">
       <Head>
         <title>World Trip</title>
       </Head>
@@ -15,6 +16,18 @@ export default function Home() {
       <Banner />
 
       <TravelTypes />
-    </>
+
+      <Divider w={90} mb="52px" borderColor="#47585B" border="1px" />
+
+      <Text
+        align="center"
+        color="#47585B"
+        fontSize="2.25rem"
+        fontWeight="500"
+        mb="52px"
+      >
+        Vamos nessa? <br /> Então escolha seu continente
+      </Text>
+    </Flex>
   );
 }
