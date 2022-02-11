a61b6477478ddc738d588e885d71241dcf71d09f
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-11 07:19:49 -0300

    chore: configure chakra-ui and fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme, Theme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+      },
+    },
+    fonts: {
+      body: 'Poppins',
+      heading: 'Poppins',
+    },
+  },
+  colors: {
+    gray: {
+      '50': '#F5F8FA',
+    },
+  },
+} as Theme | {});
