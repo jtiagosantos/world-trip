eecd31ffc027935c6731ea060fc0e0ac942b432d
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-14 17:20:42 -0400

    feat: add continent banner component

---
+++ src/components/ContinentBanner/ContinentBanner.tsx
@@ -0,0 +1,27 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+interface ContinentBannerProps {
+  continentName: string;
+  continentImageUrl: string;
+}
+
+export const ContinentBanner: React.FC<ContinentBannerProps> = ({
+  continentName,
+  continentImageUrl,
+}) => {
+  return (
+    <Flex
+      maxW={1440}
+      h={500}
+      align="flex-end"
+      bgImage={continentImageUrl}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Text color="#F5F8FA" fontSize="3rem" fontWeight="600" ml={140} mb={35}>
+        {continentName}
+      </Text>
+    </Flex>
+  );
+};
--- src/pages/continents/[slug].tsx
+++ src/pages/continents/[slug].tsx
@@ -1,5 +1,15 @@
+import { ContinentBanner } from '../../components/ContinentBanner/ContinentBanner';
 import { Header } from '../../components/Header/Header';
 
 export default function Continent() {
-  return <Header showLeftIcon />;
+  return (
+    <>
+      <Header showLeftIcon />
+
+      <ContinentBanner
+        continentImageUrl="https://images.unsplash.com/photo-1589262804704-c5aa9e6def89?ixlib=rb-1.2.1"
+        continentName="Europa"
+      />
+    </>
+  );
 }
