3014c88310f8f87ff7440c9f768d53110145dc55
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-14 16:52:38 -0400

    feat: componentize header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,28 @@
-import { Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { Flex, Icon } from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  showLeftIcon?: boolean;
+}
+
+export const Header: React.FC<HeaderProps> = ({ showLeftIcon = false }) => {
+  const router = useRouter();
+
+  const navigateToHomePage = () => router.push('/');
+
   return (
-    <Flex h={[50, 100]} justify="center" py={27}>
+    <Flex h={[50, 100]} align="center" justify="center" py={27}>
+      {showLeftIcon && (
+        <Icon
+          as={ChevronLeftIcon}
+          position="absolute"
+          left={100}
+          fontSize={32}
+          cursor="pointer"
+          onClick={navigateToHomePage}
+        />
+      )}
       <img src="/assets/images/logo.svg" alt="logo" />
     </Flex>
   );
