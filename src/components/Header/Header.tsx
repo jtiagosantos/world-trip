6b5d12ed03e6170ec784de13689a93218fe37a0e
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-14 16:52:38 -0400

    feat: componentize header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,21 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon } from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  showLeftIcon?: boolean;
+}
+
+export const Header: React.FC<HeaderProps> = ({ showLeftIcon = false }) => {
   return (
-    <Flex h={[50, 100]} justify="center" py={27}>
+    <Flex h={[50, 100]} align="center" justify="center" py={27}>
+      {showLeftIcon && (
+        <Icon
+          as={ChevronLeftIcon}
+          position="absolute"
+          fontSize={32}
+          left={100}
+        />
+      )}
       <img src="/assets/images/logo.svg" alt="logo" />
     </Flex>
   );
