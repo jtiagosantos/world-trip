a52521d0dcbdb2c02aa073ccc68318f4db348f6a
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-16 00:55:26 -0400

    add new formatter

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,2 +1,22 @@
-export const formatFirstLetterToUpperCase = (string: string) =>
-  string.charAt(0).toUpperCase() + string.slice(1);
+const formatCapitalizeString = (string: string) => {
+  const stringWithoutDashed = string.split('-');
+  stringWithoutDashed[2] =
+    stringWithoutDashed[2].charAt(0).toUpperCase() +
+    stringWithoutDashed[2].slice(1);
+
+  const capitalizedString = stringWithoutDashed.join(' ');
+
+  return capitalizedString;
+};
+
+export const formatFirstLetterToUpperCase = (string: string) => {
+  const formattedString = string.charAt(0).toUpperCase() + string.slice(1);
+
+  if (formattedString.includes('-')) {
+    const newFormattedString = formatCapitalizeString(formattedString);
+
+    return newFormattedString;
+  }
+
+  return formattedString;
+};
