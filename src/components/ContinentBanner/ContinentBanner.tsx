980b3440ecd2f9295e7c4884c8ea38b142af6374
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-15 23:22:17 -0400

    styles: set linear-gradient in background

--- src/components/ContinentBanner/ContinentBanner.tsx
+++ src/components/ContinentBanner/ContinentBanner.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 interface ContinentBannerProps {
   continentName: string;
@@ -10,19 +10,25 @@ export const ContinentBanner: React.FC<ContinentBannerProps> = ({
   continentImageUrl,
 }) => {
   return (
-    <Flex
+    <Box
       maxW={1440}
       h={500}
       mx="auto"
-      align="flex-end"
       bgImage={continentImageUrl}
       bgPosition="center"
       bgRepeat="no-repeat"
       bgSize="cover"
     >
-      <Text color="#F5F8FA" fontSize="3rem" fontWeight="600" ml={140} mb={35}>
-        {continentName}
-      </Text>
-    </Flex>
+      <Flex
+        w="100%"
+        h="100%"
+        bg="linear-gradient(0deg, rgba(28, 20, 1, 0.35), rgba(28, 20, 1, 0.35))"
+        align="flex-end"
+      >
+        <Text color="#F5F8FA" fontSize="3rem" fontWeight="600" ml={140} mb={35}>
+          {continentName}
+        </Text>
+      </Flex>
+    </Box>
   );
 };
